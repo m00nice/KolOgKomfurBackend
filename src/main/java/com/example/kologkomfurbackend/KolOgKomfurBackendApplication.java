a22a44981cec1c307e2675a4b9f9dcaeae058f27
m00ni <m00nice@live.com>
2022-11-21 10:49:02 +0100

    Initial commit

---
+++ src/main/java/com/example/kologkomfurbackend/KolOgKomfurBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kologkomfurbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolOgKomfurBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolOgKomfurBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kologkomfurbackend/KolOgKomfurBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kologkomfurbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolOgKomfurBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
