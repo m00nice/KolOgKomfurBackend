ccf0e173c41ae8ae81652caea1cfaba1fa8ef9ef
gnismas <csamsinges@gmail.com>
2022-11-22 11:24:51 +0100

    One To Many / Many To One added

--- src/main/java/com/example/kologkomfurbackend/Model/Product.java
+++ src/main/java/com/example/kologkomfurbackend/Model/Product.java
@@ -1,11 +1,9 @@
 package com.example.kologkomfurbackend.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -13,8 +11,18 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+
     private String name;
     private String skuName;
     private Double price;
+    //evt lav composition
+    private Double height;
+    private Double length;
+    private Double width;
+    //
     private String description;
+
+    @JsonBackReference
+    @ManyToOne //Many Products to One Brand
+    private Brand brand;
 }
